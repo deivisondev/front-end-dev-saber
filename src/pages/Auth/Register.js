41047f0bcfad4901bb5677eb534bab5a1a63212a
Deivison <deivisoneditordevideos@gmail.com>
2022-12-04 23:10:44 -0300

    Implementação do redux de Register

--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -3,11 +3,34 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import DevSaber from "../../assets/dev-saber.svg";
 import BackGroundImage from "../../assets/images/background-image.jpg";
+import { useSelector, useDispatch } from "react-redux";
+import { register, reset } from "../../slices/authSlice";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const { loading, error } = useSelector((state) => state.auth);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+    };
+
+    console.log(user);
+
+    dispatch(register(user));
   };
+
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
 
   return (
     <div className="auth-container">
@@ -17,10 +40,24 @@ const Register = () => {
           <h1>Dev Saber</h1>
         </div>
         <form className="auth-form" onSubmit={handleSubmit}>
-          <input type="text" placeholder="Nome" />
-          <input type="text" placeholder="Sobrenome" />
-          <input type="email" placeholder="E-mail" />
-          <input type="password" placeholder="Senha" />
+          <input
+            type="text"
+            placeholder="Nome"
+            onChange={(e) => setName(e.target.value)}
+            value={name || ""}
+          />
+          <input
+            type="email"
+            placeholder="E-mail"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email || ""}
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password || ""}
+          />
           <input id="auth-submit" type="submit" value="Cadastrar" />
         </form>
         <Link to="/login">Já estou cadastrado</Link>
